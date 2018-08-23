c191897eed420d1922edbf5bfe275d56e1c97779
AlfieriChou <alfierichou@gmail.com>
2018-08-23 13:30:51 +0800

    add config model

---
+++ config/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+const rfs = require('rotating-file-stream')
+const _ = require('lodash')
+const development = require('./config.default')
+const production = require('./config.prod')
+
+const env = process.env.NODE_ENV || 'development'
+const configs = {
+  development: development,
+  production: production
+}
+const defaultConfig = {
+  env: env
+}
+const config = _.merge(defaultConfig, configs[env])
+
+const daily = (new Date()).getFullYear() + '-' + (((new Date()).getMonth() + 1) < 10 ? '0' + ((new Date()).getMonth() + 1) : ((new Date()).getMonth() + 1)) + (new Date()).getDate()
+const logDirectory = path.join(__dirname, 'logs')
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+config.accessLogStream = rfs(daily + 'access.log', {
+  interval: '1d',
+  path: logDirectory
+})
+
+module.exports = config
--- server.js
+++ server.js
@@ -3,26 +3,16 @@ const BodyParser = require('body-parser')
 const router = require('./routes/index')
 const morgan = require('morgan')
 const engines = require('consolidate')
-const path = require('path')
-const fs = require('fs')
-const rfs = require('rotating-file-stream')
+const config = require('./config/index')
 
 const app = express()
-const port = process.env.PORT || 4000
 
 app.use(BodyParser.urlencoded({extended: true}))
 app.use(BodyParser.json())
 
-const daily = (new Date()).getFullYear() + '-' + (((new Date()).getMonth() + 1) < 10 ? '0' + ((new Date()).getMonth() + 1) : ((new Date()).getMonth() + 1)) + (new Date()).getDate()
-const logDirectory = path.join(__dirname, 'logs')
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
-const accessLogStream = rfs(daily + 'access.log', {
-  interval: '1d',
-  path: logDirectory
-})
 app.use(morgan('dev'))
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" ":response-time" ms', {
-  stream: accessLogStream
+  stream: config.accessLogStream
 }))
 
 app.use(express.static(__dirname + '/public'))
@@ -38,7 +28,7 @@ app.use((req, res, next) => {
 
 app.use('/', router)
 
-app.listen(port)
-console.log(`listening on port ${port}`)
+app.listen(config.port)
+console.log(`listening on port ${config.port}`)
 
 module.exports = app
