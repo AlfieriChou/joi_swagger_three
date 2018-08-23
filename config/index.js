83c76e5747563b1286a3158ec457779b858ec6f7
AlfieriChou <alfierichou@gmail.com>
2018-08-23 13:33:24 +0800

    fixed logs dir path

--- config/index.js
+++ config/index.js
@@ -16,7 +16,7 @@ const defaultConfig = {
 const config = _.merge(defaultConfig, configs[env])
 
 const daily = (new Date()).getFullYear() + '-' + (((new Date()).getMonth() + 1) < 10 ? '0' + ((new Date()).getMonth() + 1) : ((new Date()).getMonth() + 1)) + (new Date()).getDate()
-const logDirectory = path.join(__dirname, 'logs')
+const logDirectory = path.join(__dirname, '../logs')
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
 config.accessLogStream = rfs(daily + 'access.log', {
   interval: '1d',
